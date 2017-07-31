9df179df90ad2b357a60d62aafcef46feb76cc8f
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-07-31 13:47:01 +0600

    add loading logger from utils

--- src/main/kotlin/com/github/insanusmokrassar/HandlersSystem/Main.kt
+++ src/main/kotlin/com/github/insanusmokrassar/HandlersSystem/Main.kt
@@ -1,6 +1,6 @@
 package com.github.insanusmokrassar.HandlersSystem
 
-import com.github.insanusmokrassar.iobjectk.loadLoggerConfig
+import com.github.insanusmokrassar.utils.loadLoggerConfig
 
 fun main(args: Array<String>) {
     args.forEach {
